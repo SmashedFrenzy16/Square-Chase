a9809806a3a624fa388e970198d4b72ceec423ba
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-06-27 20:32:28 +0100

    Update square-chase.py

--- square-chase.py
+++ square-chase.py
@@ -26,3 +26,54 @@ def gameWindow():
   
   pygame.draw.rect(screen, (0, 255, 0), (x, y, 30, 30))
   pygame.draw.rect(screen, (255, 0, 0), (enemyX, enemyY, 30, 30))
+  pygame.display.update()
+  
+run = True
+
+while run:
+  
+  pygame.time.delay(100)
+  
+  if enemyX < x - 10:
+    
+    enemyX = enemyX + enemyVel
+    
+  elif enemyX > x + 10:
+    
+    enemyX = enemyX - enemyVel
+    
+   if enemyY < y - 10:
+    
+    enemyY = enemyY - enemyVel
+    
+  elif enemyY > y + 10:
+    
+    enemyY = enemyY + enemyVel
+    
+  else:
+    
+    run = False
+    
+  for event in pygame.event.get():
+    
+    if event.type == pygame.QUIT:
+      
+      run = False
+  
+  keys = pygame.key.get_pressed()
+  
+  if keys[pygame.K_LEFT]:
+            x -= vel
+
+  if keys[pygame.K_RIGHT]:
+        x += vel
+      
+  if keys[pygame.K_UP]:
+        y += vel
+      
+  if keys[pygame.K_DOWN]:
+        y -= vel
+      
+  gameWindow()
+  
+pygame.quit()
