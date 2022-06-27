31a66f1197672c5afe2a9866a00af1fbd2603048
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-06-27 19:51:02 +0100

    Update square-chase.py

--- square-chase.py
+++ square-chase.py
@@ -2,3 +2,27 @@ import pygame
 
 pygame.init()
 
+
+WIDTH = 1280
+HEIGHT = 720
+
+screen = pygame.display.setmode((WIDTH, HEIGHT))
+
+pygame.display.setcaption("Square Chase")
+
+x = 100
+y = 100
+
+enemyX = 300
+enemyY = 300
+
+vel = 5
+
+enemyVel = 6
+
+def gameWindow():
+  
+  screen.fill((0, 0, 0))
+  
+  pygame.draw.rect(screen, (0, 255, 0), (x, y, 30, 30))
+  pygame.draw.rect(screen, (255, 0, 0), (enemyX, enemyY, 30, 30))
